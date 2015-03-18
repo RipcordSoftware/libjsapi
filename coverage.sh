c88552ea620af068f270ea6ef7e98d022aa69f9c
craigminihan <craig@ripcordsoftware.com>
2015-03-18 17:08:25 +0000

    Added a check for lcov in the coverage script

--- coverage.sh
+++ coverage.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+which lcov > /dev/null
+if [ $? -ne 0 ]; then
+   echo "You must have lcov installed to run this script."
+   echo "Install with 'yum install lcov' or 'apt-get install lcov'"
+   exit 1
+fi
+
 lcov -b src/libjsapi --directory src/libjsapi/build/Debug/GNU-Linux-x86/ --capture --output-file coverage.info
 lcov --remove coverage.info 'tests/*' '/usr/*' '*/externals/*' --output-file coverage.info
 lcov --list coverage.info
