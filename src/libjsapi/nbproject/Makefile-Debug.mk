8770f9b68039bb417478ef54d1a9749ca4b80f1a
craigminihan <craig@ripcordsoftware.com>
2015-04-13 22:45:25 +0100

    Added native function tests

--- src/libjsapi/nbproject/Makefile-Debug.mk
+++ src/libjsapi/nbproject/Makefile-Debug.mk
@@ -48,6 +48,7 @@ TESTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tests
 # Test Files
 TESTFILES= \
 	${TESTDIR}/TestFiles/f5 \
+	${TESTDIR}/TestFiles/f6 \
 	${TESTDIR}/TestFiles/f3 \
 	${TESTDIR}/TestFiles/f4 \
 	${TESTDIR}/TestFiles/f2 \
@@ -124,6 +125,16 @@ ${TESTDIR}/TestFiles/f5: ${TESTDIR}/tests/call_js_function_tests.o ${OBJECTFILES
 	${MKDIR} -p ${TESTDIR}/TestFiles
 	${LINK.cc} --coverage  -o ${TESTDIR}/TestFiles/f5 $^ ${LDLIBSOPTIONS} -lpthread -ldl `pkg-config --libs zlib`   
 
+${TESTDIR}/TestFiles/f6: ../../externals/installed/lib/libmozjs-31.a
+
+${TESTDIR}/TestFiles/f6: ../../externals/installed/lib/libgtest.a
+
+${TESTDIR}/TestFiles/f6: ../../externals/installed/lib/libgtest_main.a
+
+${TESTDIR}/TestFiles/f6: ${TESTDIR}/tests/call_native_function_tests.o ${OBJECTFILES:%.o=%_nomain.o}
+	${MKDIR} -p ${TESTDIR}/TestFiles
+	${LINK.cc} --coverage  -o ${TESTDIR}/TestFiles/f6 $^ ${LDLIBSOPTIONS} -lpthread -ldl `pkg-config --libs zlib`   
+
 ${TESTDIR}/TestFiles/f3: ../../externals/installed/lib/libmozjs-31.a
 
 ${TESTDIR}/TestFiles/f3: ../../externals/installed/lib/libgtest.a
@@ -169,6 +180,12 @@ ${TESTDIR}/tests/call_js_function_tests.o: tests/call_js_function_tests.cpp
 	${MKDIR} -p ${TESTDIR}/tests
 	${RM} "$@.d"
 	$(COMPILE.cc) -g -I../../externals/installed/include/mozjs-31 -I../../externals/installed/include -I. -std=c++11 --coverage -MMD -MP -MF "$@.d" -o ${TESTDIR}/tests/call_js_function_tests.o tests/call_js_function_tests.cpp
+
+
+${TESTDIR}/tests/call_native_function_tests.o: tests/call_native_function_tests.cpp 
+	${MKDIR} -p ${TESTDIR}/tests
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I../../externals/installed/include/mozjs-31 -I../../externals/installed/include -I. -std=c++11 --coverage -MMD -MP -MF "$@.d" -o ${TESTDIR}/tests/call_native_function_tests.o tests/call_native_function_tests.cpp
 
 
 ${TESTDIR}/tests/multi_context_tests.o: tests/multi_context_tests.cpp 
@@ -278,6 +295,7 @@ ${OBJECTDIR}/value_nomain.o: ${OBJECTDIR}/value.o value.cpp
 	@if [ "${TEST}" = "" ]; \
 	then  \
 	    ${TESTDIR}/TestFiles/f5 || true; \
+	    ${TESTDIR}/TestFiles/f6 || true; \
 	    ${TESTDIR}/TestFiles/f3 || true; \
 	    ${TESTDIR}/TestFiles/f4 || true; \
 	    ${TESTDIR}/TestFiles/f2 || true; \
---
+++ src/libjsapi/tests/call_native_function_tests.cpp
@@ -0,0 +1,118 @@
+#include <gtest/gtest.h>
+
+#include <thread>
+
+#include "../libjsapi.h"
+
+rs::jsapi::Runtime rt_;
+
+class CallNativeFunctionTests : public ::testing::Test {
+protected:
+    virtual void SetUp() {
+        
+    }
+    
+    virtual void TearDown() {
+        
+    }
+    
+public:
+    static bool GetTheAnswer(JSContext* cx, unsigned argc, JS::Value* vp);
+    static bool GetLorem(JSContext* cx, unsigned argc, JS::Value* vp);
+    static bool Echo(JSContext* cx, unsigned argc, JS::Value* vp);
+};
+
+bool CallNativeFunctionTests::GetTheAnswer(JSContext* cx, unsigned argc, JS::Value* vp) {
+    auto args = JS::CallArgsFromVp(argc, vp);
+    args.rval().setInt32(42);
+    return true;
+}
+
+bool CallNativeFunctionTests::GetLorem(JSContext* cx, unsigned argc, JS::Value* vp) {
+    auto args = JS::CallArgsFromVp(argc, vp);
+
+    JS::RootedString str(cx, JS_NewStringCopyZ(cx, "Lorem ipsum"));
+    args.rval().setString(str);
+    return true;
+}
+
+bool CallNativeFunctionTests::Echo(JSContext* cx, unsigned argc, JS::Value* vp) {
+    auto args = JS::CallArgsFromVp(argc, vp);
+
+    if (argc > 0) {
+        auto value = args.get(0);
+        args.rval().set(value);
+    } else {    
+        args.rval().setNull();
+    }
+    return true;
+}
+
+TEST_F(CallNativeFunctionTests, test1) {
+    auto context = rt_.NewContext();
+    
+    JS_DefineFunction(*context, context->getGlobal(), "getTheAnswer", &CallNativeFunctionTests::GetTheAnswer, 0, 0);
+    
+    rs::jsapi::Value result(*context);
+    context->Evaluate("(function(){return getTheAnswer();})();", result);
+    
+    ASSERT_TRUE(result().isInt32());
+    ASSERT_EQ(42, result().toInt32());    
+}
+
+TEST_F(CallNativeFunctionTests, test2) {
+    auto context = rt_.NewContext();
+    
+    JS_DefineFunction(*context, context->getGlobal(), "getLorem", &CallNativeFunctionTests::GetLorem, 0, 0);
+    
+    rs::jsapi::Value result(*context);
+    context->Evaluate("(function(){return getLorem();})();", result);
+    
+    ASSERT_TRUE(result().isString());
+    ASSERT_STREQ("Lorem ipsum", result.ToString().c_str());
+}
+
+TEST_F(CallNativeFunctionTests, test3) {
+    auto context = rt_.NewContext();
+    
+    JS_DefineFunction(*context, context->getGlobal(), "echo", &CallNativeFunctionTests::Echo, 1, 0);
+    
+    rs::jsapi::FunctionArguments args(*context);
+    args.Append(true);
+    
+    rs::jsapi::Value result(*context);
+    context->Call("echo", args, result); 
+    
+    ASSERT_TRUE(result().isBoolean());
+    ASSERT_TRUE(result().toBoolean());
+}
+
+TEST_F(CallNativeFunctionTests, test4) {
+    auto context = rt_.NewContext();
+    
+    JS_DefineFunction(*context, context->getGlobal(), "echo", &CallNativeFunctionTests::Echo, 1, 0);
+    
+    rs::jsapi::FunctionArguments args(*context);
+    args.Append(42);
+    
+    rs::jsapi::Value result(*context);
+    context->Call("echo", args, result); 
+    
+    ASSERT_TRUE(result().isNumber());
+    ASSERT_EQ(42, result().toNumber());
+}
+
+TEST_F(CallNativeFunctionTests, test5) {
+    auto context = rt_.NewContext();
+    
+    JS_DefineFunction(*context, context->getGlobal(), "echo", &CallNativeFunctionTests::Echo, 1, 0);
+    
+    rs::jsapi::FunctionArguments args(*context);
+    args.Append("hello");
+    
+    rs::jsapi::Value result(*context);
+    context->Call("echo", args, result); 
+    
+    ASSERT_TRUE(result().isString());
+    ASSERT_STREQ("hello", result.ToString().c_str());
+}
