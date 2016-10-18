dec601176e81a0cf9b8ece9a16cb9d49ea96b762
craigminihan <craig@ripcordsoftware.com>
2016-10-18 12:34:13 +0100

    Added build matrix for clang on linux builds

---
+++ .travis/before_script.sh
@@ -0,0 +1,14 @@
+mkdir -p .travis.temp
+pushd .travis.temp
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    curl http://ftp.uk.debian.org/debian/pool/main/l/lcov/lcov_1.11.orig.tar.gz -O
+    tar xfz lcov_1.11.orig.tar.gz
+    mkdir -p lcov && make -C lcov-1.11/ install PREFIX=~/lcov
+    export PATH=~/lcov/usr/bin:$PATH
+    gem install coveralls-lcov
+elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    brew update
+fi
+
+popd
